14a6185f9f6d62faa910fb64f6eba1898b5f1b15
elamir <othmaniamir41@gmail.com>
2026-02-14 12:50:13 +0100

    Fix auto-updater import and bump version

--- electron/main.js
+++ electron/main.js
@@ -8,7 +8,8 @@ import { spawn } from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
-import { autoUpdater } from "electron-updater";
+import electronUpdater from "electron-updater";
+const { autoUpdater } = electronUpdater;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
